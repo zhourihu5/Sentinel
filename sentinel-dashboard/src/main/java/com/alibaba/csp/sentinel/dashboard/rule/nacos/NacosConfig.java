528cad3b78782e02613a08097931146e6d32ef57
a-zhourihu <rihu.zhou@dhc.com.cn>
2021-08-07 12:32:17 +0800

    changed to nacos configed

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
@@ -15,16 +15,17 @@
  */
 package com.alibaba.csp.sentinel.dashboard.rule.nacos;
 
-import java.util.List;
-
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
 import com.alibaba.csp.sentinel.datasource.Converter;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.config.ConfigFactory;
 import com.alibaba.nacos.api.config.ConfigService;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Properties;
 
 /**
  * @author Eric Zhao
@@ -32,6 +33,9 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class NacosConfig {
+
+    @Value("${nacos.address}")
+    private String address;
 
     @Bean
     public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
@@ -45,6 +49,8 @@ public class NacosConfig {
 
     @Bean
     public ConfigService nacosConfigService() throws Exception {
-        return ConfigFactory.createConfigService("localhost");
+        Properties properties = new Properties();
+        properties.put("serverAddr",address);
+        return ConfigFactory.createConfigService(properties);
     }
 }
